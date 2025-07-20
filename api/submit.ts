be6b789ab62df279f371d09ffe8c526b50b07eb3
Shobhit Agarwal <shobhit@stripe.com>
2025-07-20 22:38:54 +0530

    fix

---
+++ api/submit.ts
@@ -0,0 +1,21 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
+
+const GOOGLE_SCRIPT_URL = process.env.GOOGLE_SCRIPT_URL;
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  console.log("Received request:", req.body);
+  if (req.method !== "POST") {
+    return res.status(200).json({ error: "Method Not Allowed" });
+  }
+  const response = await axios.post(GOOGLE_SCRIPT_URL, req.body, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    timeout: 30000, // 30 second timeout
+  });
+
+  res.json(response.data);
+}
