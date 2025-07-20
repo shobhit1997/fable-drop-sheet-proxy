9c4585aba86e857fa691e1801b8694d30f856390
Shobhit Agarwal <shobhit@stripe.com>
2025-07-20 23:11:37 +0530

    fix

--- api/submit.ts
+++ api/submit.ts
@@ -5,25 +5,46 @@ dotenv.config();
 
 const GOOGLE_SCRIPT_URL = process.env.GOOGLE_SCRIPT_URL;
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  console.log("Received request:", req.body);
-  if (req.method !== "POST") {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Authorization"
-    );
-    res.setHeader("Content-Type", "application/json");
-    res.status(405).json({ error: "Method Not Allowed" });
-    return res;
-  }
-  const response = await axios.post(GOOGLE_SCRIPT_URL, req.body, {
+export async function OPTIONS() {
+  return new Response(null, {
+    status: 200,
     headers: {
-      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      "Access-Control-Allow-Credentials": "true",
     },
-    timeout: 30000, // 30 second timeout
   });
-
-  res.json(response.data);
 }
+
+export async function GET() {
+  return Response.json(
+    { ok: true },
+    {
+      headers: { "Access-Control-Allow-Origin": "*" },
+    }
+  );
+}
+
+// export default async function handler(req: VercelRequest, res: VercelResponse) {
+//   console.log("Received request:", req.body);
+//   if (req.method !== "POST") {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+//     res.setHeader(
+//       "Access-Control-Allow-Headers",
+//       "Content-Type, Authorization"
+//     );
+//     res.setHeader("Content-Type", "application/json");
+//     res.status(200).json({ error: "Method Not Allowed" });
+//     return res;
+//   }
+//   const response = await axios.post(GOOGLE_SCRIPT_URL, req.body, {
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     timeout: 30000, // 30 second timeout
+//   });
+
+//   res.json(response.data);
+// }
