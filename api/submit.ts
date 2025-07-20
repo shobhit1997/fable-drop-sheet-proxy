e4cfcc4090a79c1f6c6bff1597ec458f7f75df2f
Shobhit Agarwal <shobhit@stripe.com>
2025-07-20 23:04:41 +0530

    fix

--- api/submit.ts
+++ api/submit.ts
@@ -8,16 +8,15 @@ const GOOGLE_SCRIPT_URL = process.env.GOOGLE_SCRIPT_URL;
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   console.log("Received request:", req.body);
   if (req.method !== "POST") {
-    return res
-      .status(200)
-      .headers({
+    return res.status(200).json({
+      error: "Method Not Allowed",
+      headers: {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
         "Access-Control-Allow-Headers": "Content-Type, Authorization",
-      })
-      .json({
-        error: "Method Not Allowed",
-      });
+        "Content-Type": "application/json",
+      },
+    });
   }
   const response = await axios.post(GOOGLE_SCRIPT_URL, req.body, {
     headers: {
