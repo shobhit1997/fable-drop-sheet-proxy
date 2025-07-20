e452aae607e0c066452c672083ae1fa2a78ee519
Shobhit Agarwal <shobhit@stripe.com>
2025-07-20 23:13:37 +0530

    fix

--- api/submit.ts
+++ api/submit.ts
@@ -21,30 +21,28 @@ export async function GET() {
   return Response.json(
     { ok: true },
     {
-      headers: { "Access-Control-Allow-Origin": "*" },
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        "Access-Control-Allow-Credentials": "true",
+      },
     }
   );
 }
 
-// export default async function handler(req: VercelRequest, res: VercelResponse) {
-//   console.log("Received request:", req.body);
-//   if (req.method !== "POST") {
-//     res.setHeader("Access-Control-Allow-Origin", "*");
-//     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-//     res.setHeader(
-//       "Access-Control-Allow-Headers",
-//       "Content-Type, Authorization"
-//     );
-//     res.setHeader("Content-Type", "application/json");
-//     res.status(200).json({ error: "Method Not Allowed" });
-//     return res;
-//   }
-//   const response = await axios.post(GOOGLE_SCRIPT_URL, req.body, {
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     timeout: 30000, // 30 second timeout
-//   });
+export async function POST(req: VercelRequest, res: VercelResponse) {
+  console.log("Received request:", req.body);
+  const response = await axios.post(GOOGLE_SCRIPT_URL, req.body, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    timeout: 30000, // 30 second timeout
+  });
 
-//   res.json(response.data);
-// }
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Content-Type", "application/json");
+  res.json(response.data);
+}
